06e3379305fc96e09abbec403d559643c9aa8647
Nilabja Roy <nilabjaroy2001@gmail.com>
2024-02-08 02:21:04 +0530

    fr

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Recommendation X ",
+    page_icon="🎮🕹)",
+)
+
+st.title("Recommendation X")
+#st.sidebar.success("Choose your categories:")
+st.write("During the last few decades, with the rise of Youtube, Amazon, Netflix, and many other such web services, recommender systems have taken more and more place in our lives.")
+
+st.write("From e-commerce (suggest to buyers articles that could interest them) to online advertisement (suggest to users the right contents, matching their preferences), recommender systems are today unavoidable in our daily online journeys.")
+
+st.write("In a very general way, recommender systems are algorithms aimed at suggesting relevant items to users (items being movies to watch, text to read, products to buy, or anything else depending on industries).Recommender systems are really critical in some industries as they can generate a huge amount of income when they are efficient or also be a way to stand out significantly from competitors.")
+
+st.write("~ The main objective is to create a collection of recommendation systems for users.")
